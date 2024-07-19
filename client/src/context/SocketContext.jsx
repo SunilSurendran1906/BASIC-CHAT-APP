8372b22d8db415f4934d9e7f8a64de5d9aad4ff8
SUNIL SURENDRAN <133184647+SunilSurendran1906@users.noreply.github.com>
2024-07-19 14:26:23 +0530

    commit

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000", {
+			const socket = io("https://basic-chat-app-56mw.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
